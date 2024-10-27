d06fe40341d6e95a67e4c1a729f7e05564a5d78e
Josh_corgi <whvkek@naver.com>
2024-10-28 02:09:34 +0900

    [add] : RoomReserveService 테스트 코드 완료

--- src/main/java/com/example/library_management/domain/roomReserve/controller/RoomReserveController.java
+++ src/main/java/com/example/library_management/domain/roomReserve/controller/RoomReserveController.java
@@ -8,7 +8,6 @@ import com.example.library_management.domain.roomReserve.dto.response.RoomReserv
 import com.example.library_management.domain.roomReserve.dto.response.RoomReserveUpdateResponseDto;
 import com.example.library_management.domain.roomReserve.service.RoomReserveService;
 import com.example.library_management.global.security.UserDetailsImpl;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +24,7 @@ public class RoomReserveController {
     public ResponseEntity<RoomReserveCreateResponseDto> createRoomReserve(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                           @PathVariable Long roomId,
                                                                           @RequestBody RoomReserveCreateRequestDto roomReserveCreateRequestDto) {
-        return ResponseEntity.ok(roomReserveService.createRoomReserve(userDetails, roomId, roomReserveCreateRequestDto));
+        return ResponseEntity.ok(roomReserveService.createRoomReserve(userDetails.getUser(), roomId, roomReserveCreateRequestDto));
     }
 
     @PatchMapping("/library/rooms/{roomId}/reservations/{reserveId}")
@@ -36,14 +35,14 @@ public class RoomReserveController {
         // (예약 시작일, 예약 종료일) 둘 중 최소 하나 이상의 데이터의 전달 유무 검증.
         roomReserveUpdateRequestDto.validate();
 
-        return ResponseEntity.ok(roomReserveService.updateRoomReserve(userDetails, roomId, reserveId, roomReserveUpdateRequestDto));
+        return ResponseEntity.ok(roomReserveService.updateRoomReserve(userDetails.getUser(), roomId, reserveId, roomReserveUpdateRequestDto));
     }
 
     @DeleteMapping("/library/rooms/{roomId}/reservations/{reserveId}")
     public ResponseEntity<RoomReserveDeleteResponseDto> deleteRoomReserve(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                           @PathVariable Long roomId,
                                                                           @PathVariable Long reserveId) {
-        return ResponseEntity.ok(roomReserveService.deleteRoomReserve(userDetails, roomId, reserveId));
+        return ResponseEntity.ok(roomReserveService.deleteRoomReserve(userDetails.getUser(), roomId, reserveId));
     }
 
     @GetMapping("/library/rooms/{roomId}/reservations")
@@ -58,6 +57,6 @@ public class RoomReserveController {
                                                                       @RequestParam(defaultValue = "10") int size,
                                                                       @AuthenticationPrincipal UserDetailsImpl userDetails,
                                                                       @PathVariable Long roomId) {
-        return ResponseEntity.ok(roomReserveService.findAllRoomReserveByUser(page, size, userDetails, roomId));
+        return ResponseEntity.ok(roomReserveService.findAllRoomReserveByUser(page, size, userDetails.getUser(), roomId));
     }
 }
